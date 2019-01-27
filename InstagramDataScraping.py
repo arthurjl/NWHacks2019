c36c33a56a3611ac4d44f9d1362917fb2f552b91
Jake Jung <junyong217@hotmail.com>
2019-01-27 11:28:44 -0800

    Updated InstagramDataScraping

--- InstagramDataScraping.py
+++ InstagramDataScraping.py
@@ -9,75 +9,68 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
-browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+def get_data():
+	browser = webdriver.Firefox()
+	browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
 
-# Find username field
-usernameField = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
+	# Find username field
+	usernameField = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
 
-# Find password field
-passwordField = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
+	# Find password field
+	passwordField = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
 
-usernameField.send_keys('LETZy33tTHEWh34t')
-passwordField.send_keys('arthuristhenewjake')
+	usernameField.send_keys('LETZy33tTHEWh34t')
+	passwordField.send_keys('arthuristhenewjake')
 
-# Find login button
-loginButton = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Log in"]')))
-loginButton.click()
+	# Find login button
+	loginButton = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Log in"]')))
+	loginButton.click()
 
-# grab by css selector
-# article > image
+	# grab by css selector
+	# article > image
 
 
-for i in range(3):
-	srcList = []
-	time.sleep(5)
-	pictures = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "article > div img")))
-	for picture in pictures:
-		srcList.append(picture.get_attribute("src"))
+	for i in range(3):
+		srcList = []
+		time.sleep(5)
+		pictures = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "article > div img")))
+		for picture in pictures:
+			srcList.append(picture.get_attribute("src"))
 
-	nameList = []
-	names = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "article header h2 > a")))
-	for name in names:
-		nameList.append(name.get_attribute("title"))
+		nameList = []
+		names = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "article header h2 > a")))
+		for name in names:
+			nameList.append(name.get_attribute("title"))
 
-	likedList = []
-	hearts = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "article div section div a > span")))
-	# article > div > section > div > span > span (temp new selector)
+		likedList = []
+		hearts = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "article div section div a > span")))
+		# article > div > section > div > span > span (temp new selector)
 
-	# article div section div a > span (old selector for likes)
-	for heart in hearts:
-		likedList.append(heart.text)
+		# article div section div a > span (old selector for likes)
+		for heart in hearts:
+			likedList.append(heart.text)
 
-	while (len(likedList) < len(srcList)):
-		likedList.append("n/a")
+		while (len(likedList) < len(srcList)):
+			likedList.append("n/a")
 
-	print(srcList)
-	print(len(srcList))
-	print(nameList)
-	print(len(nameList))
-	print(likedList)
-	print(len(likedList))
-
-	htmlElem = browser.find_element_by_tag_name('html')
-	htmlElem.send_keys(Keys.END)
+		htmlElem = browser.find_element_by_tag_name('html')
+		htmlElem.send_keys(Keys.END)
 
 
 
 
-postList = []
-for i in range(len(srcList)):
-	thisdict =	{
-		"src": srcList[i],
-		"poster": nameList[i],
-		"likes": likedList[i]
-	}
-	postList.append(thisdict)
+	postList = []
+	for i in range(len(srcList)):
+		thisdict =	{
+			"src": srcList[i],
+			"poster": nameList[i],
+			"likes": likedList[i]
+		}
+		postList.append(thisdict)
 
-print(postList)
+	return postList
 
-for src in postList:
-	webbrowser.open(postList[i].get('src'))
+
 
 
 
--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from PIL import Image
 import requests
 from io import BytesIO
 import matplotlib.pyplot as plt
+from InstragramDataScraping import get_data
 
 G=nx.star_graph(0)
 
 # Posts == array of dictionaries/objects given by selenium
-posts = [{'src': 'https://instagram.fyvr1-1.fna.fbcdn.net/vp/2901cae5f91c3749859c3135b94ac860/5CE8DE19/t51.2885-15/e35/50300096_301480393758229_3987981158001670975_n.jpg?_nc_ht=instagram.fyvr1-1.fna.fbcdn.net', 'poster': 'instagram', 'likes': '379,095'}, {'src': 'https://instagram.fyvr1-1.fna.fbcdn.net/vp/a823d923266d9e06b62478ef5991190d/5CE7B8A1/t51.2885-15/e35/49753212_233226567611508_395983073781492838_n.jpg?_nc_ht=instagram.fyvr1-1.fna.fbcdn.net', 'poster': 'prattprattpratt', 'likes': '142,845'}, {'src': 'https://instagram.fyvr1-1.fna.fbcdn.net/vp/3c4f5ba1f68615180c8bdb041891664d/5C4FFC5E/t51.2885-15/e15/49792400_293877844820682_2398668401181985046_n.jpg?_nc_ht=instagram.fyvr1-1.fna.fbcdn.net', 'poster': 'chrisbrownofficial', 'likes': 'n/a'}]
+# posts = [{'src': 'https://instagram.fyvr1-1.fna.fbcdn.net/vp/2901cae5f91c3749859c3135b94ac860/5CE8DE19/t51.2885-15/e35/50300096_301480393758229_3987981158001670975_n.jpg?_nc_ht=instagram.fyvr1-1.fna.fbcdn.net', 'poster': 'instagram', 'likes': '379,095'}, {'src': 'https://instagram.fyvr1-1.fna.fbcdn.net/vp/a823d923266d9e06b62478ef5991190d/5CE7B8A1/t51.2885-15/e35/49753212_233226567611508_395983073781492838_n.jpg?_nc_ht=instagram.fyvr1-1.fna.fbcdn.net', 'poster': 'prattprattpratt', 'likes': '142,845'}, {'src': 'https://instagram.fyvr1-1.fna.fbcdn.net/vp/3c4f5ba1f68615180c8bdb041891664d/5C4FFC5E/t51.2885-15/e15/49792400_293877844820682_2398668401181985046_n.jpg?_nc_ht=instagram.fyvr1-1.fna.fbcdn.net', 'poster': 'chrisbrownofficial', 'likes': 'n/a'}]
 
 # pictureArray = []
 # for i, post in enumerate(posts):
@@ -21,6 +22,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+	posts = get_data()
 	return render_template('index.html', posts = posts)
 	
 
