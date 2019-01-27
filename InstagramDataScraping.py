2bda673e336f5ff677344e074e0b14778ed72320
arthurliu <theonlyarthurliu@gmail.com>
2019-01-26 17:51:18 -0800

    Login To Instagram
    TODO: Click the login button

--- InstagramDataScraping.py
+++ InstagramDataScraping.py
@@ -7,5 +7,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Firefox()
-driver.get('https://www.instagram.com/?hl=en')
+browser = webdriver.Firefox()
+browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+
+# Find username field
+usernameField = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
+
+# Find password field
+passwordField = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
+
+usernameField.send_keys('itsarthurliu')
+passwordField.send_keys('ryanmvp')
+
+# Find login button
+loginButton = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Log in')))
+loginButton.click();
+
