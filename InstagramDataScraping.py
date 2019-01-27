3190af69a0e44a9e7c09295e6506cf53f312a110
Jake Jung <junyong217@hotmail.com>
2019-01-26 21:16:02 -0800

    Update InstagramDataScraping.py
    Updated data scraper to take data from posts only

--- InstagramDataScraping.py
+++ InstagramDataScraping.py
@@ -2,6 +2,7 @@
 # InstagramDataScraping.py
 # Scrapes through Instagram to collect posts and post data
 
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,4 +23,17 @@ passwordField.send_keys('ryanmvp')
 # Find login button
 loginButton = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Log in"]')))
 loginButton.click()
+
+# grab by css selector
+# article > image
+
+srcList = []
+while len(srcList) < 20:
+	time.sleep(5)
+	pictures = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "article > div img")))
+	for picture in pictures:
+		srcList.append(picture.get_attribute("src"))
+
+print(srcList)
+
 
