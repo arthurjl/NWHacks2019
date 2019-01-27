9057995b880dd77ae751f5cc789fa25f3490fd56
arthurliu <theonlyarthurliu@gmail.com>
2019-01-27 11:23:37 -0800

    Instafeed - Friendzone
    Name change

---
+++ InstaPost.py
@@ -0,0 +1,12 @@
+class InstaPost:
+	def __init__(image, poster, likes):
+		# url of image
+		self.image = image
+		# name of poster
+		self.poster = poster
+		# number of likes on post
+		self.likes = likes
+
+		# TODO
+		# Number of followers and followed
+		# Mutual following (more of a negative if they aren't)
